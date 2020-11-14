5ed61d63b6b948aea6dce7d7f7a9634dc0c6a961
Angel-Garcia-Aranda <agararan@everis.com>
2020-11-14 14:20:07 +0100

    1 file modified

--- script.js
+++ script.js
@@ -5,17 +5,22 @@ const changeBackgroundButton = document.getElementById(
 );
 
 changeBackgroundButton.addEventListener("click", function() {
-  this.style.backgroundColor = getRandomColor();
+  this.style.backgroundColor = ()=> {const letters = ()=>'0123456789ABCDEF'
+    let color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+     }
+   return color;}
 });
 
-function getRandomColor() {
-  const letters = ()=>'0123456789ABCDEF' ;
-  let color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
+// function getRandomColor() {
+//   const letters = ()=>'0123456789ABCDEF' ;
+//   let color = '#';
+//   for (var i = 0; i < 6; i++) {
+//     color += letters[Math.floor(Math.random() * 16)];
+//   }
+//   return color;
+// }
 
 // function getLetters(){
 //   return '0123456789ABCDEF';
